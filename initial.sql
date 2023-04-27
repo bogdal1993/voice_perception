aa35a09764f478fe53da843c3fca66c8c5eedc26
bogdal1993 <boltik1993@gmail.com>
2023-04-27 15:12:57 +0300

    first commit

---
+++ initial.sql
@@ -0,0 +1,128 @@
+
+CREATE SCHEMA vp;
+
+
+SET default_tablespace = '';
+
+SET default_with_oids = false;
+
+--
+-- Name: calls; Type: TABLE; Schema: vp; Owner: -
+--
+
+CREATE TABLE vp.calls (
+    call_uuid uuid NOT NULL,
+    call_start_ts timestamp with time zone,
+    call_end_ts timestamp with time zone,
+    caller character varying,
+    calle character varying,
+    duration integer,
+    direction character varying
+);
+
+
+--
+-- Name: calls_meta; Type: TABLE; Schema: vp; Owner: -
+--
+
+CREATE TABLE vp.calls_meta (
+    call_uuid uuid NOT NULL,
+    meta jsonb
+);
+
+
+--
+-- Name: calls_transcription; Type: TABLE; Schema: vp; Owner: -
+--
+
+CREATE TABLE vp.calls_transcription (
+    call_uuid uuid NOT NULL,
+    transcription jsonb
+);
+
+
+--
+-- Name: files; Type: TABLE; Schema: vp; Owner: -
+--
+
+CREATE TABLE vp.files (
+    call_uuid uuid NOT NULL,
+    file_server character varying,
+    file_path text,
+    num_channels smallint
+);
+
+
+--
+-- Name: tasks; Type: TABLE; Schema: vp; Owner: -
+--
+
+CREATE TABLE vp.tasks (
+    call_uuid uuid NOT NULL,
+    task jsonb
+);
+
+
+--
+-- Name: transcript_queue; Type: TABLE; Schema: vp; Owner: -
+--
+
+CREATE TABLE vp.transcript_queue (
+    file_server character varying,
+    file_path text,
+    status character varying,
+    call_uuid uuid NOT NULL
+);
+
+
+--
+-- Name: calls_meta calls_meta_pkey; Type: CONSTRAINT; Schema: vp; Owner: -
+--
+
+ALTER TABLE ONLY vp.calls_meta
+    ADD CONSTRAINT calls_meta_pkey PRIMARY KEY (call_uuid);
+
+
+--
+-- Name: calls calls_pkey; Type: CONSTRAINT; Schema: vp; Owner: -
+--
+
+ALTER TABLE ONLY vp.calls
+    ADD CONSTRAINT calls_pkey PRIMARY KEY (call_uuid);
+
+
+--
+-- Name: calls_transcription calls_transcription_pkey; Type: CONSTRAINT; Schema: vp; Owner: -
+--
+
+ALTER TABLE ONLY vp.calls_transcription
+    ADD CONSTRAINT calls_transcription_pkey PRIMARY KEY (call_uuid);
+
+
+--
+-- Name: files files_pkey; Type: CONSTRAINT; Schema: vp; Owner: -
+--
+
+ALTER TABLE ONLY vp.files
+    ADD CONSTRAINT files_pkey PRIMARY KEY (call_uuid);
+
+
+--
+-- Name: tasks tasks_pkey; Type: CONSTRAINT; Schema: vp; Owner: -
+--
+
+ALTER TABLE ONLY vp.tasks
+    ADD CONSTRAINT tasks_pkey PRIMARY KEY (call_uuid);
+
+
+--
+-- Name: transcript_queue transcript_queue_pkey; Type: CONSTRAINT; Schema: vp; Owner: -
+--
+
+ALTER TABLE ONLY vp.transcript_queue
+    ADD CONSTRAINT transcript_queue_pkey PRIMARY KEY (call_uuid);
+
+
+--
+-- PostgreSQL database dump complete
+--
---
+++ worker_node/text_processor/textprocessor.py
@@ -0,0 +1,76 @@
+from deeppavlov import build_model, configs
+import os, time, json
+from psycopg2 import pool
+import rutokenizer
+import rupostagger
+import rulemma
+
+
+lemmatizer = rulemma.Lemmatizer()
+lemmatizer.load()
+
+tokenizer = rutokenizer.Tokenizer()
+tokenizer.load()
+
+tagger = rupostagger.RuPosTagger()
+tagger.load()
+
+DSN = os.getenv('DSN')
+threaded_postgreSQL_pool = pool.ThreadedConnectionPool(1, 3, DSN)
+
+#model = build_model(configs.classifiers.rusentiment_bert, download=False)
+model = build_model(configs.classifiers.rusentiment_convers_bert, download=False)
+
+print("ff", flush=True)
+print(model(['алло'])[0], flush=True)
+
+tokens = tokenizer.tokenize('народных')
+tags = tagger.tag(tokens)
+lemmas = lemmatizer.lemmatize(tags)
+print(lemmas[0][2], lemmas[0][3],  flush=True)
+
+def process_call_transcript(call_uuid,task_data):
+	conn = threaded_postgreSQL_pool.getconn()
+	cur = conn.cursor()
+	cur.execute("SELECT transcription FROM vp.calls_transcription where call_uuid = %s limit 1",(call_uuid,))
+	result = cur.fetchone()
+	cur.close()
+	threaded_postgreSQL_pool.putconn(conn)
+	transcription = result[0]
+	for frase in transcription:
+		frase['emotion'] = model([frase['text']])[0]
+		try:
+			for word in frase['result']:
+				tokens = tokenizer.tokenize(word['word'])
+				tags = tagger.tag(tokens)
+				lemmas = lemmatizer.lemmatize(tags)
+				word['lemma'] = lemmas[0][2]
+				word['part'] = lemmas[0][3]
+		except:
+			pass
+	conn = threaded_postgreSQL_pool.getconn()
+	cur = conn.cursor()
+	cur.execute("UPDATE vp.calls_transcription SET transcription = %s where call_uuid = %s",(json.dumps(transcription), call_uuid,))
+	task_data['text_process'] = "OK"
+	cur.execute("UPDATE vp.tasks SET task = %s where call_uuid = %s",(json.dumps(task_data), call_uuid,))
+	conn.commit()
+	cur.close()
+	threaded_postgreSQL_pool.putconn(conn)
+	
+
+while 1:
+	conn = threaded_postgreSQL_pool.getconn()
+	cur = conn.cursor()
+	cur.execute("SELECT call_uuid, task FROM vp.tasks where task->>'text_process' = 'ready' limit 30")
+	tasks = cur.fetchall()
+	cur.close()
+	threaded_postgreSQL_pool.putconn(conn)
+	i = 0
+	for task in tasks:
+		i+=1
+		call_uuid,task_data = task
+		process_call_transcript(call_uuid,task_data)
+		#cur.execute("update vp.transcript_queue set status = 'processing' where call_uuid = %s",(call_uuid,))
+		#conn.commit()
+	if i==0:
+		time.sleep(5)
